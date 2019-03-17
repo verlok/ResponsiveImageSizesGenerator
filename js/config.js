9dffc807a48ae8af20ebd32b6c7d17a2ef6ea99a
verlok <andrea.verlicchi@gmail.com>
2019-03-17 17:04:00 +0100

    Added devices to config

--- js/config.js
+++ js/config.js
@@ -1,5 +1,47 @@
 export default {
 	numberOfProducts: 20,
+	viewportsToOptimizeFor: [
+		{
+			name: "iPhone 5/SE",
+			width: 320,
+			pixelDensity: 2
+		},
+		{
+			name: "iPhone 6/7/8",
+			width: 375,
+			pixelDensity: 2
+		},
+		{
+			name: "iPhone X",
+			width: 375,
+			pixelDensity: 3
+		},
+		{
+			name: "iPad portrait",
+			width: 768,
+			pixelDensity: 2
+		},
+		{
+			name: "iPad landscape",
+			width: 1024,
+			pixelDensity: 2
+		},
+		{
+			name: "iPad Pro landscape",
+			width: 1366,
+			pixelDensity: 2
+		},
+		{
+			name: "Laptop MDpi",
+			width: 1280,
+			pixelDensity: 1
+		},
+		{
+			name: "Laptop HiDpi",
+			width: 1440,
+			pixelDensity: 2
+		}
+	],
 	media: [
 		{
 			minWidth: 0,
