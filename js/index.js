a0c75830c5097cf4976496e607df9ac63c1f191f
verlok <andrea.verlicchi@gmail.com>
2019-03-18 07:26:16 +0100

    Moved MQs to JS generated (still static)

--- js/index.js
+++ js/index.js
@@ -12,3 +12,20 @@ var $products = document.querySelector(".products");
 $products.innerHTML = products.reduce((previousHtml, product) => {
 	return previousHtml + productTemplate(product);
 }, "");
+
+// STYLE CREATION
+
+var $stylesheet = document.getElementById("generatedStylesheet");
+$stylesheet.innerHTML = `.product {
+	width: 50%;
+}
+@media (min-width: 768px) {
+	.product {
+		width: 33.333%;
+	}
+}
+@media (min-width: 1024px) {
+	.product {
+		width: 25%;
+	}
+}`;
