a38f6d53760e1a61f555357ea26d8215ad732f16
Andrea Verlicchi <andrea.verlicchi@ynap.com>
2019-03-19 15:14:38 +0100

    Product images space holder

--- js/getStylesheetHtml.js
+++ js/getStylesheetHtml.js
@@ -1,8 +1,18 @@
 import { mediaQueryTemplate } from "./templates/style.js";
 
-export default config =>
-	config.media.reduce(
+const productMediaQueries = mediaConfig =>
+	mediaConfig.reduce(
 		(previousHtml, mediaQ) =>
 			previousHtml + "\n" + mediaQueryTemplate(mediaQ),
 		""
 	);
+
+const productHoldHeight = config => {
+	return `.product-images {
+		padding-bottom: ${100 / config.imageRatio}%;
+	}`;
+};
+
+export default config => {
+	return productHoldHeight(config) + "\n" + productMediaQueries(config.media);
+};
