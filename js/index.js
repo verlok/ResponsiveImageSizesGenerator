42e978167a5120171c0d94822d143f263096bca7
verlok <andrea.verlicchi@gmail.com>
2019-03-28 06:47:05 +0100

    Moved config to root

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-import config from "./config.js";
+import config from "/config.js";
 import buildProductsModel from "./buildProductsModel.js";
 import getProductsHtml from "./getProductsHtml.js";
 import getStylesheetHtml from "./getStylesheetHtml.js";
